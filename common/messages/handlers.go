d4259a96d3d793c0b57ba6ee7430e072b423e636
Saurabh Badhwar <sbadhwar@redhat.com>
2018-01-07 11:06:51 +0530

    Add a generic message handler
    Signed-off-by: Saurabh Badhwar <sbadhwar@redhat.com>

--- common/messages/handlers.go
+++ common/messages/handlers.go
@@ -8,22 +8,26 @@ import (
 	"fmt"
 )
 
+// MessageHandler defines the type for function signature to be used by the
+// message handlers.
+type MessageHandler func(string)([]byte, error)
+
 // MessageHandlers defines a type to register different message
 // handlers respective of the types they are supposed to handle.
 // For a new message handler, they have to provide the type of message
 // they can handle and the handler function that should be called
 // with the payload.
 type MessageHandlers struct {
-	handlers map[MessageType]func(string)([]byte, error)
+	handlers map[MessageType]MessageHandler
 }
 
 // NewMessageHandler returns a new MessageHandlers instance to be worked upon
 func NewMessageHandler() *MessageHandlers {
-	return &MessageHandlers{handlers: make(map[MessageType]func(string)([]byte, error))}
+	return &MessageHandlers{handlers: make(map[MessageType]MessageHandler)}
 }
 
 // RegisterHandler registers a new message handler
-func (mh *MessageHandlers) RegisterHandler(mType MessageType, handler func(string)([]byte, error)) error {
+func (mh *MessageHandlers) RegisterHandler(mType MessageType, handler MessageHandler) error {
 	if _, ok := mh.handlers[mType]; ok {
 		return fmt.Errorf("Message handler already exists for the provided type")
 	}
@@ -41,9 +45,16 @@ func (mh *MessageHandlers) UnregisterHandler(mType MessageType) error {
 }
 
 // GetHandler returns the message handler for the provided message type
-func (mh MessageHandlers) GetHandler(mType MessageType) (func(string)([]byte, error), error) {
+func (mh MessageHandlers) GetHandler(mType MessageType) (MessageHandler, error) {
 	if handler, ok := mh.handlers[mType]; ok {
 		return handler, nil
 	}
 	return nil, fmt.Errorf("Unable to find a handler for the specified message type")
+}
+
+// GenericHandler defines a generic way of dealing with the incoming message
+// payloads.
+func GenericHandler(payload string) ([]byte, error) {
+	fmt.Println(payload)
+	return []byte(payload), nil
 }
